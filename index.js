226bc5d3e7d62d2193a255332d371c1aae2f4d05
Utkarsh724 <utkarshrawat72483@gmail.com>
2023-09-08 05:17:29 +0530

    Add game function

--- index.js
+++ index.js
@@ -20,33 +20,69 @@ function playRound( playerSelection,computerSelection )
     let caseInsensitivePS = playerSelection.toLowerCase();
     if( caseInsensitivePS === "rock" && computerSelection === "paper")
     {
-        return "You lose! Paper beats Rock";
+        console.log("You lose! Paper beats Rock");
+        return "loss";
     }
     else if( caseInsensitivePS === "paper" && computerSelection === "scissors")
     {
-        return "You lose! Scissors beats Paper";
+        console.log("You lose! Scissors beats Paper");
+        return "loss";
     }
     else if( caseInsensitivePS === "scissors" && computerSelection === "rock")
     {
-        return "You lose! Rock beats Scissors";
+        console.log("You lose! Rock beats Scissors");
+        return "loss";
     }
     else if( caseInsensitivePS === "rock" && computerSelection === "scissors")
     {
-        return "You Win! Rock beats Scissors";
+        console.log("You Win! Rock beats Scissors");
+        return "win";
     }
     else if( caseInsensitivePS === "paper" && computerSelection === "rock")
     {
-        return "You Win! Paper beats Rock";
+        console.log("You Win! Paper beats Rock");
+        return "win";
     }
     else if( caseInsensitivePS === "scissors" && computerSelection === "paper")
     {
-        return "You Win! Scissors beats Paper";
+        console.log("You Win! Scissors beats Paper");
+        return "win";
     }
     else
     {
-        return "Its a draw"
+        console.log("Its a draw");
+        return "draw";
     }
 }
 
+function game()
+{
+    let user = 0, comp = 0;
+    for(let i = 0; i<5; i++)
+    {
+        let userChoice = prompt("Choose between Rock, Paper and Scissors");
+        let Result = playRound( userChoice, getComputerChoice());
+        if(Result==="win")
+        {
+            user++;
+        }
+        else if(Result==="loss")
+        {
+            comp++
+        }
+    }
+    if(comp>user)
+    {
+        console.log("Computer Wins")
+    }
+    else if(comp<user)
+    {
+        console.log("User Wins");
+    }
+    else
+    {
+        console.log("Its a tie");
+    }
+}
 
-
+game();
